4afc1229dd87cdc6a5f5eb1f25da0f2954206813
Unite.cash <unitecash@gmail.com>
2018-05-24 08:25:30 -0700

    reconfigure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,131 @@
 const path = require('path')
-const webpack = require("webpack")
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const exclude = /(node_modules|tests)/
+const webpack = require('webpack')
 
-module.exports = {
+const config = {
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist')
+  },
   entry: {
     main: "./src/client/js/main.ts",
     login: "./src/client/js/login.ts"
   },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, 'public/js')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ],
+        exclude: exclude
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: false,
+                sourceMap: true
+              }
+            }
+          ]
+        }),
+        exclude: exclude
+      },
+      {
+        test: /\.(png|jpg|ico|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ],
+        exclude: exclude
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ],
+        exclude: exclude
+      },
+      {
+        test: /\.txt$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ],
+        exclude: exclude
+      },
+      {
+        test: /\.(mp3|wav|ogg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'audio/'
+            }
+          }
+        ],
+        exclude: exclude
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ],
+        exclude: exclude
+      }
+    ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: 'src/index.html'
+    // }),
+    // new FaviconsWebpackPlugin('images/favicon/favicon.png'),
+    new ExtractTextPlugin({
+      filename: (getPath) => {
+        return getPath('css/styles.css')
+      }
+    }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
@@ -19,4 +134,6 @@ module.exports = {
   node: {
     fs: 'empty'
   }
-};
+}
+
+module.exports = config
