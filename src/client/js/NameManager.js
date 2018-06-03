92ad7f000e58646e131ac66ab0068b6f75cf6527
Unite.cash <unitecash@gmail.com>
2018-06-02 21:26:48 -0700

    Create Name and NetworkManager

---
+++ src/client/js/NameManager.js
@@ -0,0 +1,15 @@
+/**
+ * NameManager
+ * Author: The Unite.cash Developers
+ * License: GNU AGPL v3
+ *
+ * A class for querying, caching, storing, and retrieving Name objects
+ *
+ * @file Defines the NameManager class
+ */
+
+ export default class NameManager {
+
+
+
+ }
---
+++ src/client/js/NetworkManager.js
@@ -0,0 +1,25 @@
+/**
+ * Network Manager
+ * Author: The Unite.cash Developers
+ * License: GNU AGPL v3
+ *
+ * Establishes and manages connections with block explorers, WebSocket Servers
+ * and WebTorrent seeders.
+ *
+ * @file Defines the NetworkManager class
+ */
+
+export default class NetworkManager {
+
+  /*
+  * @constructor
+  *
+  * Set up and connect to networks, block explorers and WebSockets
+  *
+  */
+
+  constructor() {
+    
+  }
+
+}
